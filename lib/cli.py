aaa7e51feee2be219a4cb05982982a14017709e1
Amrita <amritak.saw@gmail.com>
2024-12-05 12:33:32 -0500

    added create profile feature

--- lib/cli.py
+++ lib/cli.py
@@ -1,10 +1,14 @@
 from lib.skelly_hi import skelly_hi
 from lib.skelly_bye import skelly_bye
+from time import sleep
+from lib.models.models import User
+# , Directive
+import ipdb
 
 
 class App:
     def __init__(self):
-        # Book.create_table()
+        User.create_table()
         # Review.create_table()
         self.input = None
     
@@ -27,6 +31,7 @@ class App:
 
         print("Please choose an option to opt out of optional suffering:")
         print(main_menu_opts)
+        self.input = None
 
         while self.input not in ["1", "2", "3", "4", "5", "6"]:
 
@@ -35,23 +40,23 @@ class App:
             if self.input == "1":
                 self.manage_user()
                 sleep(2)
-                print(printed_options)
+                print(main_menu_opts)
             elif self.input == "2":
                 self.advanced_directive_setup() 
                 sleep(2)
-                print(printed_options)
+                print(main_menu_opts)
             elif self.input == "3":
                 self.organize_funds()
                 sleep(2)
-                print(printed_options)
+                print(main_menu_opts)
             elif self.input == "4":
                 self.handle_digital()
                 sleep(2)
-                print(printed_options)
+                print(main_menu_opts)
             elif self.input == "5":
                 self.task_overview() 
                 sleep(2)
-                print(printed_options)
+                print(main_menu_opts)
             if self.input == "6":
                 self.exit_program()
             else:
@@ -65,16 +70,36 @@ class App:
         4. Return to Main Menu
         """
         print(user_menu_opts)
+        self.input = None
 
+        while self.input not in ["1", "2", "3", "4"]:
+            self.input = input(">>> ")
+
+            if self.input == "1":
+                self.new_profile()
+            elif self.input == "4":
+                self.return_main()
+            else:
+                print("Please choose a number based on the menu options")   
+        
     def advanced_directive_setup(self):
         advanced_directive_opts = """
         1. Create New Directive
         2. Delete Directive
         3. View All Directives
-        4. Find Directive by Representative Name
-        5. Return to Main Menu
+        4. Return to Main Menu
         """
         print(advanced_directive_opts)
+        self.input = None
+
+        while self.input not in ["1", "2", "3", "4"]:
+
+            self.input = input(">>> ")
+
+            if self.input == "4":
+                self.return_main()
+            else:
+                print("Please choose a number based on the menu options")          
 
     def organize_funds(self):
         fund_opts = """
@@ -83,6 +108,16 @@ class App:
         3. Return to Main Menu
         """
         print(fund_opts)
+        self.input = None
+
+        while self.input not in ["1", "2", "3"]:
+
+            self.input = input(">>> ")
+
+            if self.input == "3":
+                self.return_main()
+            else:
+                print("Please choose a number based on the menu options")
 
     def handle_digital(self):
         digital_opts = """
@@ -91,6 +126,16 @@ class App:
         3. Return to Main Menu     
         """
         print(digital_opts)
+        self.input = None
+
+        while self.input not in ["1", "2", "3"]:
+
+            self.input = input(">>> ")
+
+            if self.input == "3":
+                self.return_main()
+            else:
+                print("Please choose a number based on the menu options")
 
     def task_overview(self):
         task_opts = """
@@ -99,9 +144,56 @@ class App:
         3. Return to Main Menu
         """
         print(task_opts)
+        self.input = None
 
+        while self.input not in ["1", "2", "3"]:
+
+            self.input = input(">>> ")
+
+            if self.input == "3":
+                self.return_main()
+            else:
+                print("Please choose a number based on the menu options")
+
+#THE BASIC NAV FUNCTIONS
     def exit_program(self):
         print(skelly_bye)
         print("   Have fun dying,")
         print("   Goodbye!")
         exit()
+
+    def return_main(self):
+        self.input = None
+        self.main_menu()
+
+#USER PROFILE FUNCTIONS
+    def new_profile(self):
+        print("Enter your FIRST name (Use the name that appears on your legal documents):")
+        self.input = None
+        while not self.input or not (3 <= len(self.input) <= 20):
+            self.input = input(">>> ")
+            if self.input or not (3 <= len(self.input) <= 20): 
+                first_name = self.input
+            else:
+                print ("Invalid input...")
+
+        print("Enter your LAST name (Use the name that appears on your legal documents):")
+        self.input = None
+        while not self.input or not (2 <= len(self.input) <= 20):
+            self.input = input (f">>> {first_name} ")
+            if not self.input == (f">>> {first_name} ") or not (3 <= len(self.input) <= 30): 
+                last_name = self.input 
+            else: 
+                print ("Invalid input...")
+        
+        new_user = User(first_name = first_name, last_name = last_name)
+        new_user.save()
+
+        print(f"A profile has been created for {first_name} {last_name}! You can now create and link Advanced Directives to {first_name}. YAY!")
+        
+        self.input = None
+
+        print(" ")
+        sleep(3)
+        self.manage_user()
+        
--- lib/models/models.py
+++ lib/models/models.py
@@ -1,2 +1,111 @@
 from . import CONN, CURSOR
 
+class User:
+    def __init__(self, first_name:str, last_name:str, age:int=None, id:int=None):
+        self.id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age 
+
+    def __repr__(self):
+        return f"User(id={self.id}, first_name={self.first_name}, last_name={self.last_name} )"
+    
+    @classmethod
+    def create_table(cls):
+        sql = """
+        CREATE TABLE IF NOT EXISTS users (
+        id INTEGER PRIMARY KEY,
+        first_name TEXT NOT NULL,
+        last_name TEXT NOT NULL, 
+        age INTEGER
+        )
+        """
+        CONN.execute(sql)
+
+    @classmethod
+    def get_all(cls):
+        sql = """
+        SELECT * FROM users
+        """
+
+        user_data = CURSOR.execute(sql).fetchall()
+
+        user_instances = [User(
+            id=u[0],
+            first_name=u[1],
+            last_name=u[2],
+            age=u[3]
+        ) for u in user_data ]
+
+        return user_instances
+    
+# CRUD METHODS
+    def create(self):
+        sql = """
+        INSERT INTO users (first_name, last_name, age)
+        VALUES (?, ?, ?)
+        """
+        CURSOR.execute(sql, [self.first_name, self.last_name, self.age])
+        CONN.commit()
+
+        last_row_sql = """
+        SELECT id FROM users
+        ORDER BY id DESC
+        LIMIT 1
+        """
+
+        self.id = CURSOR.execute(last_row_sql).fetchone()[0]
+
+        return self
+ 
+    def update(self):
+        sql = """
+        UPDATE users
+        SET first_name = ?, last_name = ?, age = ?
+        WHERE id = ?
+        """
+
+        CURSOR.execute(sql, [self.first_name, self.last_name, self.age, self.id])
+        CONN.commit()
+
+        return self
+
+    def save(self):
+        if self.id:
+            self.update()
+        else:
+            self.create()
+        return self
+    
+    def destroy(self):
+        sql = """
+        DELETE FROM users
+        where id = ?
+        """
+
+        CURSOR.execute(sql, [self.id])
+        CONN.commit()
+
+        self.id = None
+
+#JOIN METHODS
+    # def reviews(self):
+    #     sql = """
+    #     SELECT * FROM reviews
+    #     WHERE book_id = ?
+    #     """
+
+    #     review_tuples = CURSOR.execute(sql, [self.id]).fetchall()
+
+    #     review_instances = [ Review(
+    #         id=rv[0], 
+    #         stars=rv[1], 
+    #         username=rv[2], 
+    #         book_id=rv[3]
+    #     ) for rv in review_tuples ]
+
+    #     return review_instances
+
+
+class Directive:
+    pass 
